f20a73e2b3e6a7643179279eecc88f872e321eb1
Ahmed Belghith <abelghith@oakland.edu>
2025-06-23 11:38:29 -0400

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -143,7 +143,7 @@ export const projects = [
   {
   id: nanoid(),
   img: img4, // Replace with a relevant image or screenshot of the app
-  url: 'https://your-deployment-url.com', // Replace with the actual deployment link 
+  url: 'https://www.societe3c.online', // Replace with the actual deployment link 
   github: 'https://github.com/AhmedBelghith24/Paycheck-Management-System',
   title: 'Paycheck Management System 💰',
   text: 'This is a full-stack Paycheck Management System built with the MERN stack. It allows users to track, filter, and mark paychecks as paid, with support for supplier, bank, and monthly filters.'
